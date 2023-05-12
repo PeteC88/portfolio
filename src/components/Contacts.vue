c4330525f4857c03d3c7108b79c5b35fd5cacba5
PeteC88 <40523064+PeteC88@users.noreply.github.com>
2023-05-12 16:37:12 +0200

    Add files via upload

---
+++ src/components/Contacts.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="contacts">
+        <h1>Restons en contact</h1>
+        <div class="contacts__logos">
+            <div class="contacts__logo">
+                <font-awesome-icon :icon="['fab', 'linkedin']" class="contact__icon" />
+                <p>LinkedIn</p>
+            </div>
+            <div class="contacts__logo">
+                <font-awesome-icon :icon="['fab', 'github']" class="contact__icon"/>
+                <p>E-mail</p>
+            </div>
+            <div class="contacts__logo">
+                <font-awesome-icon :icon="['fab', 'github']" class="contact__icon"/>
+                <p> GitHub</p>
+            </div>
+            <div class="contacts__logo">
+                <font-awesome-icon :icon="['fas', 'file-arrow-down']" class="contact__icon"/>
+                <p>Télécharge mon CV</p>
+            </div>
+
+        </div>
+    </div>
+</template>
